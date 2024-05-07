171430ea061b68524e75c1d7c23720b4c41aa22d
malla-lokesh <121810307046@gitam.in>
2024-05-07 18:05:53 +0530

    able read text file and print it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>Hello world!</div>;
+  const [file, setFile] = useState();
+  const [content, setContent] = useState("");
+
+  const reader = new FileReader();
+
+  reader.addEventListener(
+    "load",
+    () => {
+      setContent(reader.result);
+    },
+    false
+  );
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+
+    if (selectedFile) reader.readAsText(selectedFile);
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="file"
+          name="input"
+          id="input"
+          onChange={handleFileChange}
+        />
+      </div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default App;
